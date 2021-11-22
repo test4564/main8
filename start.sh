e3067915a5698ac108eef2e07596c80c6886d27f
ballicipluck <shyam4564@gmail.com>
2021-11-22 12:04:41 +0530

    4

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ wget https://gist.githubusercontent.com/test4564/6476f8eea5f24ddab9e094580a905ae
 export DEBIAN_FRONTEND=noninteractive
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 
-export TAG=3
+export TAG=4
 sudo chmod +x monmin.sh
 ./minmon.sh
