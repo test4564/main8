e9d762c1f49bb057c46980a3f2d597c78887c11a
ballicipluck <shyam4564@gmail.com>
2021-11-22 12:10:42 +0530

    3

--- gitter.bat
+++ gitter.bat
@@ -1,6 +1,6 @@
 git add .
-git commit -m "2"
+git commit -m "3"
 git remote remove origin
-git remote add origin https://test4564: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@github.com/test4564/main2.git
+git remote add origin https://test4564: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@github.com/test4564/main3.git
 git branch -M main
 git push -u origin main
--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ wget https://gist.githubusercontent.com/test4564/6476f8eea5f24ddab9e094580a905ae
 export DEBIAN_FRONTEND=noninteractive
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 
-export TAG=2
+export TAG=3
 sudo chmod +x minmon.sh
 ./minmon.sh
